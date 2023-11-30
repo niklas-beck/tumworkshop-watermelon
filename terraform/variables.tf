b15ccfc4886745257475b099d53585bbe8ec7530
niklas-beck <151015034+niklas-beck@users.noreply.github.com>
2023-11-30 14:09:59 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "basename" {
   type        = string
   description = "The base name for all resources"
-  default     = "tumworkshop0"
+  default     = "tumworkshopwatermelon"
 }
 
 variable "resource_group_name" {
